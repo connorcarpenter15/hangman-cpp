203a5eb8cadfaf67ddb990a92984d93dad7f1a2b
nicolas <nicolasecrespo@gmail.com>
2023-11-17 13:01:44 -0500

    added checkwinlose

--- src/checkWinLose/checkWinLose.cpp
+++ src/checkWinLose/checkWinLose.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 
-bool checkWinLose(int limbs) {
-    if (limbs >= 6) {
-        cout << "Oh no, you have ran out of guesses!";
-
-        return false; //return false if lose
-    }
+// if terminate, return true, else return false
+bool checkWinLose(int limbs, string &word, string &status) {
+  if (limbs >= 6) {
+    cout << "Oh no, you have ran out of guesses!";
+    return true; // return false if lose
+  } else if (status == word) {
+    cout << "You won!";
+    return true;
+  } else {
+    // not win or lose
+    return false;
+  }
+  return false;
 }
