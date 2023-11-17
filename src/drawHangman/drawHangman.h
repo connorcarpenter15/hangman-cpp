356addf058cb55762986aa60c2e76b48b8d58781
whyammehere <zhangian230791@gmail.com>
2023-11-17 13:03:53 -0500

    aaaahhhhhh

--- src/drawHangman/drawHangman.h
+++ src/drawHangman/drawHangman.h
@@ -1,83 +1,10 @@
+#ifndef DRAWHANGMAN_H
+#define DRAWHANGMAN_H
+
 #include <iostream>
 
 using namespace std;
 
-void drawHangman(int limbs) {
-    cout << "You have " << 6 - limbs << " guesses left" << endl << endl;
+string drawHangman();
 
-    // cout << " ╔════╤═ " <<
-    // endl << " ║    O  " <<
-    // endl << " ║   \\|/" <<
-    // endl << " ║    |  " <<
-    // endl << " ║   / \\" <<
-    // endl << " ║       " <<
-    // endl << "═╩═══════";
-
-    if (limbs == 0) {
-        cout << " +----+- " <<
-        endl << " |       " <<
-        endl << " |       " <<
-        endl << " |       " <<
-        endl << " |       " <<
-        endl << " |       " <<
-        endl << "-+-------";
-
-    } else if (limbs == 1) {
-        cout << " +----+- " <<
-        endl << " |    O  " <<
-        endl << " |       " <<
-        endl << " |       " <<
-        endl << " |       " <<
-        endl << " |       " <<
-        endl << "-+-------";
-
-    } else if (limbs == 2) {
-        cout << " +----+- " <<
-        endl << " |    O  " <<
-        endl << " |    |  " <<
-        endl << " |    |  " <<
-        endl << " |       " <<
-        endl << " |       " <<
-        endl << "-+-------";
-
-    } else if (limbs == 3) {
-        cout << " +----+- " <<
-        endl << " |    O  " <<
-        endl << " |   \\| " <<
-        endl << " |    |  " <<
-        endl << " |       " <<
-        endl << " |       " <<
-        endl << "-+-------";
-
-    } else if (limbs == 4) {
-        cout << " +----+- " <<
-        endl << " |    O  " <<
-        endl << " |   \\|/" <<
-        endl << " |    |  " <<
-        endl << " |   /   " <<
-        endl << " |       " <<
-        endl << "-+-------";
-
-    } else if (limbs == 5) {
-        cout << " +----+- " <<
-        endl << " |    O  " <<
-        endl << " |   \\|/" <<
-        endl << " |    |  " <<
-        endl << " |   / \\" <<
-        endl << " |       " <<
-        endl << "-+-------";
-
-    } else { //limbs == 6
-
-    cout << " +----+- " <<
-    endl << " |    O  " <<
-    endl << " |   \\|/" <<
-    endl << " |    |  " <<
-    endl << " |   / \\" <<
-    endl << " |       " <<
-    endl << "-+-------";
-    }
-
-    cout << endl;
-}
-
+#endif // !DRAWHANGMAN
