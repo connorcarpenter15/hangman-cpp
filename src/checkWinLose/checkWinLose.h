fda940c46e4881cb9890f6233170c65e0c39cbef
nicolas <nicolasecrespo@gmail.com>
2023-11-17 20:51:23 -0500

    updated include statements and things

--- src/checkWinLose/checkWinLose.h
+++ src/checkWinLose/checkWinLose.h
@@ -1,6 +1,10 @@
 #ifndef CHECKWINLOSE_H
 #define CHECKWINLOSE_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
 bool checkWinLose(int limbs, string &word, string &status);
 
 #endif // !CHECKWINLOSE_H
